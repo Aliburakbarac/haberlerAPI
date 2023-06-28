57d9d297c8b68b519773ec9cc090d02ac994d4fc
Ali Burak <alibarac41@icloud.com>
2023-06-28 17:59:56 +0300

    Initial Commit

---
+++ haberler/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  haberler
+//
+//  Created by Ali Burak Baraç on 28.06.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
