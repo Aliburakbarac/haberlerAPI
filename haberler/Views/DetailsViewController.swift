3f58a8ada64ad6d90fbb523bf3d81782f56c1a09
Ali Burak <alibarac41@icloud.com>
2025-12-25 18:56:06 +0300

    Deneme
    deneme

--- haberler/Views/DetailsViewController.swift
+++ haberler/Views/DetailsViewController.swift
@@ -9,7 +9,7 @@ import UIKit
 import AVKit
 
 
-class DetailsViewController: UIViewController {
+final class DetailsViewController: UIViewController {
 
     @IBOutlet weak var haberImg: UIImageView!
     @IBOutlet weak var descriptionLabel: UILabel!
